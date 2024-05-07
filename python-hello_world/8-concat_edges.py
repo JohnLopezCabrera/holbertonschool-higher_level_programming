395a40f1ae5945509ffc090f1190c4efb3b5e9ea
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-07 13:57:59 -0400

    l

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,5 +3,4 @@ class Print:
     def __init__(self, message):
         self.message = message
         print(self.message)
-
 Print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax")
