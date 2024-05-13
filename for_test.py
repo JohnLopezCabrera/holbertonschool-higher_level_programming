07a690c57faa0e1eafcaab34caa42262784e27f6
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-13 13:38:42 -0400

    l

---
+++ for_test.py
@@ -0,0 +1,13 @@
+credit_card = "1234-5678-9012-3456"
+for z in credit_card:
+    print(z)
+for x in range (1, 11, 3):
+	print(x)
+
+print("HAPPY NEW YEAR!")
+
+for a in range(1, 21):
+    if a == 13:
+        break
+    else:
+        print(a)
---
+++ test.py
@@ -0,0 +1,29 @@
+fruits = ["apple", "orange", "banana", "coconut"]
+#list = [] ordered and changeable. Duplicates ok
+# set = {} unordered and immutable, but Add/Remove ok 
+# - cannot use indexing because they are unordered. no duplicates
+# tuple = () ordered and unchangeable. duplicates ok. FASTER
+
+# Lists [ ]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+#print (fruits.count("pineapple"))
+
+# Set { }
+#fruits.add("pineapple")
+#fruits.remove("apple")
+#fruits.clear()
+
+#tupple
+
+print(fruits)
