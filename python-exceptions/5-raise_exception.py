4d6b5c93a51020d8d14c65991d457066a05fd4a6
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-15 17:06:16 -0400

    l

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    raise_exception = __import__('5-raise_exception').raise_exception
-
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception raised")
+def raise_exception():
+    raise TypeError
