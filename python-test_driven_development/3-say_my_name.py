010f143f0a9a8087b719e438d5e7b5c52734d9ba
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-20 20:03:12 -0400

    l

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+"""
+Print string
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """Concatenate both parameters and print
+    Parameters:
+    first_name = first name
+    last_name = last name
 
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    Local Variables:
+    fullname = empty string
+
+    Return: None
+   """
+    fullname = ""
+    if first_name and type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    elif first_name and type(first_name) == str:
+        fullname += first_name + " "
+    if last_name and type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    elif last_name and type(last_name) == str:
+        fullname += last_name
+    print("My name is {:s}".format(fullname))
