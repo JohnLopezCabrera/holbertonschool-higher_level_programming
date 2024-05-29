18117e78f46aeda5b7c9a3a7f34c4d731dd05eeb
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-28 20:35:58 -0400

    l

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""Module will provide a function to write
+a string to a .txt file and
+returns the characters to be written."""
+
+
 def write_file(filename="", text=""):
     """Writes a string to a .txt and returns the number of characters.
-    
+
     Args:
         filename (str): The name of the file to write to.
         text (str): The text to write to the file.
-        
+
     Returns:
         int: The number of characters written.
     """
