bfb1fcd37a4702b24738a95a7f04678fdad8d215
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-05-28 20:15:30 -0400

    l

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function to read file"""
+
+
+def read_file(filename=""):
+    """Read a text file and prints it."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
