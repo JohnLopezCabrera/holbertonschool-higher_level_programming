bd13ebe098f5f60427cd3848704fb7a29f62b12d
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-07-26 22:42:21 -0400

    final

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const listMoviesUl = document.getElementById('list_movies');
+        data.results.forEach(movie => {
+          const listItem = document.createElement('li');
+          listItem.textContent = movie.title;
+          listMoviesUl.appendChild(listItem);
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  });
